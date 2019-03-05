b7a3ccac86685083a4c13e133e57778a70cada3e
Max Crisafulli <maxcr2801@gmail.com>
2019-03-05 09:58:59 -0800

    Unit 09 Elevens Act. 1 & 2 (partial)

---
+++ Unit 09 Elevens/src/activity2/DeckTester.java
@@ -0,0 +1,14 @@
+package activity2;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Deck test = new Deck(new String[] {"A", "B", "C"}, new String[] {"Giraffes", "Lions"}, new int[] {2,1,6});
+	}
+}
