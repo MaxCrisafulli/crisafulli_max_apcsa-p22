7554be8bb5c5422851e57f3bb562fbe43dde312c
Max Crisafulli <maxcr2801@gmail.com>
2019-03-06 11:12:18 -0800

    Activity 4 Completed

---
+++ Unit 09 Elevens/src/activity4/DeckTester.java
@@ -0,0 +1,19 @@
+package activity4;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Deck test = new Deck(new String[] {"A", "B", "C", "D", "E"}, new String[] {"Giraffes", "Lions", "Zebras"}, new int[] {2,1,6,4,5});
+		test.printCards();
+		test.shuffle();
+		test.printCards();
+		
+		
+	}
+}
