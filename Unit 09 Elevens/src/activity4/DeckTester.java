203d05b716ce397fafc13bb960272fc1a260b4f1
Max Crisafulli <maxcr2801@gmail.com>
2019-03-08 09:32:45 -0800

    Upload

--- Unit 09 Elevens/src/activity4/DeckTester.java
+++ Unit 09 Elevens/src/activity4/DeckTester.java
@@ -9,11 +9,12 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		Deck test = new Deck(new String[] {"A", "B", "C", "D", "E"}, new String[] {"Giraffes", "Lions", "Zebras"}, new int[] {2,1,6,4,5});
+		Deck test = new Deck(new String[] {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}, new String[] {"Clubs", "Spades", "Diamonds", "Hearts" }, new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13});
 		test.printCards();
+		System.out.println(test.size());
 		test.shuffle();
 		test.printCards();
-		
+		System.out.println(test.size());
 		
 	}
 }
