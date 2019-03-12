104c82d41f18cb9732f610621e33d330a2473e39
Max Crisafulli <maxcr2801@gmail.com>
2019-03-12 10:00:50 -0700

    Unit 09 Done

--- Unit 09/src/ListDown.java
+++ Unit 09/src/ListDown.java
@@ -12,6 +12,16 @@ public class ListDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(List<Integer> numArray)
 	{
-		return true;
+		boolean output = true;
+		for (int i = 0; i < numArray.size()-1; i++) {
+			if (numArray.get(i) > numArray.get(i+1)) {
+				output = true;
+			}
+			if (!(numArray.get(i) > numArray.get(i+1))) {
+				output = false;
+				break;
+			}
+		}
+		return output;
 	}	
 }
