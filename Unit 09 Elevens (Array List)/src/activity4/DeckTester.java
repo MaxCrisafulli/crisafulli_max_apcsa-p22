1b82b66e90e90b84e5147fe1a5b3b2851ae9ba9e
Max Crisafulli <33032859+MaxCrisafulli@users.noreply.github.com>
2019-03-11 18:42:14 -0700

    elevensupdate

--- Unit 09 Elevens (Array List)/src/activity4/DeckTester.java
+++ Unit 09 Elevens (Array List)/src/activity4/DeckTester.java
@@ -11,8 +11,9 @@ public class DeckTester {
 	public static void main(String[] args) {
 		Deck test = new Deck(new String[] {"A", "B", "C", "D", "E"}, new String[] {"Giraffes", "Lions", "Zebras"}, new int[] {2,1,6,4,5});
 		System.out.println(test);
-		test.shuffle();
+		test.deal();
 		System.out.println(test);
+		
 		
 	}
 }
