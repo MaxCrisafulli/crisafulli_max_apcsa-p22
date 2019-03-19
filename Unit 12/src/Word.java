880fd3d22f722990e52b140a0224803b08b3540e
Max Crisafulli <maxcr2801@gmail.com>
2019-03-19 09:51:42 -0700

    Unit 12 Partial

--- Unit 12/src/Word.java
+++ Unit 12/src/Word.java
@@ -7,18 +7,40 @@ import static java.lang.System.*;
 public class Word implements Comparable<Word>
 {
 	private String word;
+	private int length;
 
 	public Word( String s )
 	{
+		word = s;
+		length = s.length();
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	
+	public int getLength() {
+		return length;
 	}
 
 	public int compareTo( Word rhs )
-	{		
-		return 0;
+	{
+		int aSize = length;
+		int bSize = rhs.getLength();
+		
+		if (aSize > bSize) {
+			return 1;
+		}
+		else if (bSize > aSize) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
 	}
 
 	public String toString()
 	{
-		return "";
+		return word;
 	}
 }
--- Unit 12/src/WordRunner.java
+++ Unit 12/src/WordRunner.java
@@ -7,26 +7,36 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.Arrays;
 import static java.lang.System.*;
+import java.util.*;
 
 public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner(new File("words.dat"));
-
 		int size = file.nextInt();
 		file.nextLine();
 		
-
-
-
-
-
-
-
-
-
-
-
+		ArrayList<Word> list = new ArrayList<Word>();
+		
+		for (int i = 0; i < size; i++) {    //adds word data to array list
+			list.add(new Word(file.nextLine()));
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		for (int j = 0; j < list.size(); j++) { //prints array list
+			System.out.println(list.get(j));
+		}
+		
 	}
 }
