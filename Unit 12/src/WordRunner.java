31a158b03e85341d55c93d80cc52429d26517359
Max Crisafulli <maxcr2801@gmail.com>
2019-03-20 10:40:48 -0700

    Unit 12 Complete

--- Unit 12/src/WordRunner.java
+++ Unit 12/src/WordRunner.java
@@ -24,17 +24,34 @@ public class WordRunner
 		}
 		
 		
+		//sorts the list by length
+		ArrayList<Word> output = new ArrayList<Word>(); //the new output list 
+		size = list.size();
+		for (int k = 0; k < size; k ++) {
+			Word bigw = new Word("");
+			int biggestlength = 0;
+			int bigwindex = 0;
+			for (int l = 0; l < list.size(); l++) { //finds biggest word
+				if (list.get(l).compareTo(bigw) == 0) { // if word lengths are same 
+					if (list.get(l).getWord().compareTo(bigw.getWord()) > 0) {
+						biggestlength = list.get(l).getLength();
+						bigw = list.get(l);
+						bigwindex = l;
+					}
+				}
+				else if (list.get(l).compareTo(bigw) == 1) { //if there are larger words
+					biggestlength = list.get(l).getLength();
+					bigw = list.get(l);
+					bigwindex = l;
+				}
+			}
+			list.remove(bigwindex);
+			output.add(0,bigw);
+		}
+		list = output;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		for (int j = 0; j < list.size(); j++) { //prints array list
+		//prints the array list
+		for (int j = 0; j < list.size(); j++) {
 			System.out.println(list.get(j));
 		}
 		
