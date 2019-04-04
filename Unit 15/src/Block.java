8e5a2ee7eaabeb7f643edbee5b617c90b123b7bb
Max Crisafulli <maxcr2801@gmail.com>
2019-04-04 10:35:43 -0700

    Pong Partial

--- Unit 15/src/Block.java
+++ Unit 15/src/Block.java
@@ -16,57 +16,127 @@ public class Block implements Locatable
 
 	public Block()
 	{
-
-
+		setX(0);
+		setY(0);
+		setWidth(10);
+		setHeight(10);
+		setColor(Color.WHITE);
 	}
 
 	//add other Block constructors - x , y , width, height, color
 	
+	public Block(int x, int y) {
+		setX(x);
+		setY(y);
+		setWidth(10);
+		setHeight(10);
+		setColor(Color.WHITE);
+
+	}
 	
+	public Block(int x, int y, int wd) {
+		setX(x);
+		setY(y);
+		setWidth(wd);
+		setHeight(10);
+		setColor(Color.WHITE);
+	}
 	
+	public Block(int x, int y, int wd, int ht) {
+		setX(x);
+		setY(y);
+		setWidth(wd);
+		setHeight(ht);
+		setColor(Color.WHITE);
+
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+	public Block(int x, int y, int wd, int ht, Color col) {
+		setX(x);
+		setY(y);
+		setWidth(wd);
+		setHeight(ht);
+		setColor(col);
+	}
 	
 	
    //add the other set methods
+
+    public void setColor(Color col)
+    {
+   	    color = col;
+    }
    
+   public void setPos(int x, int y) {
+		xPos = x;
+		yPos = y;
+	}
 
-   public void setColor(Color col)
-   {
+	public void setX(int x) {
+		xPos = x;
+	}
 
+	public void setY(int y) {
+		yPos = y;
+	}
+	
+	public void setWidth(int w) {
+		width = w;
+	}
+	
+	public void setHeight(int h) {
+		height = h;
+	}
 
-   }
+	//add the other get methods
+	
+	public int getX() {
+		return xPos;
+	}
+
+	public int getY() {
+		return yPos;
+	} 
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
 
    public void draw(Graphics window)
    {
-   	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+      window.setColor(color);
+      window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
 
    public void draw(Graphics window, Color col)
    {
-
+	   window.setColor(col);
+	   window.fillRect(getX(), getY(), getWidth(), getHeight());
 
    }
    
-	public boolean equals(Object obj)
+	public boolean equals(Block obj)
 	{
-
-
-
-
+		if (getX() == obj.getX() && getY() == obj.getY() && getWidth() == obj.getWidth() && getHeight() == obj.getHeight() && getColor() == obj.getColor()){
+			return true;
+		}
 		return false;
-	}   
-
-   //add the other get methods
+	}
     
 
    //add a toString() method  - x , y , width, height, color
+	
+	public String toString() {
+		String output = "";
+		output += getX() + " " + getY() + " " + getWidth() + " " + getHeight() + " " + getColor(); 
+		return output;
+	}
 }
--- Unit 15/src/Paddle.java
+++ Unit 15/src/Paddle.java
@@ -13,35 +13,84 @@ public class Paddle extends Block
    public Paddle()
    {
 		super(10,10);
-      speed =5;
+		speed = 5;
    }
-
-
+   
    //add the other Paddle constructors
 
-
-
-
-
-
-
-
-
+   public Paddle(int x, int y)
+   {
+		super(x,y);
+		speed = 5;
+   }
+   
+   public Paddle(int x, int y, int wd)
+   {
+		super(x,y,wd);
+		speed = 5;
+   }
+   
+   public Paddle(int x, int y, int wd, int ht)
+   {
+		super(x,y,wd,ht);
+		speed = 5;
+   }
+   
+   public Paddle(int x, int y, int wd, int ht, Color col)
+   {
+		super(x,y,wd,ht,col);
+		speed = 5;
+   }
+   
+   public Paddle(int x, int y, int wd, int ht, Color col, int spd)
+   {
+		super(x,y,wd,ht,col);
+		speed = spd;
+   }
+   
+   public Paddle(int x, int y, int wd, int ht, int spd)
+   {
+		super(x,y,wd,ht);
+		speed = spd;
+   }
 
    public void moveUpAndDraw(Graphics window)
    {
-
-
+	   draw(window);
+	   this.setY(this.getY()+1);
    }
 
    public void moveDownAndDraw(Graphics window)
    {
-
-
+	   draw(window);
+	   this.setY(this.getY()-1);
    }
 
    //add get methods
+   public int getXSpeed() {
+	   return 5;
+   }
+   
+   public int getYSpeed() {
+	   return 6;
+   }
+   
+   public int getSpeed() {
+	   return speed;
+   }
+   
+   
+   
    
    
    //add a toString() method
+   public String toString() {
+	  String output = "";
+	  output += super.toString() + " " + getSpeed();
+	  return output;
+   }
+   
+   
+   
+   
 }
