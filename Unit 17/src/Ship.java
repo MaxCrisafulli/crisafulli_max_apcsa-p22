3adf525872ace4a2f35319b6a43b9f138f0ae0d3
Max Crisafulli <maxcr2801@gmail.com>
2019-05-03 11:10:27 -0700

    Unit 17 File Upload

---
+++ Unit 17/src/Ship.java
@@ -0,0 +1,73 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(10,10,10,10,10);
+	}
+
+	public Ship(int x, int y)
+	{
+	   super(x,y);
+	}
+
+	public Ship(int x, int y, int s)
+	{
+	   super(x,y);
+	   speed = s;
+	}
+
+	public Ship(int x, int y, int w, int h, int s)
+	{
+		super(x, y, w, h);
+		speed=s;
+		try
+		{
+			URL url = getClass().getResource("H:/APCSA/Github%20Repository/crisafulli_max_apcsa-p22/Unit%2017/ship.jpg");
+			image = ImageIO.read(url);
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void move(String direction)
+	{
+		if ()
+	}
+
+	public void draw( Graphics window )
+	{
+   	window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
